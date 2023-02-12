c3a807e3e7ab747b4ab9a01b4eed58a3426197a1
y <whyyouareay@gmail.com>
2023-02-13 01:54:55 +0900

    [feat] add brands, customers resources

--- project-nest/src/app.module.ts
+++ project-nest/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { LoggingModule } from './interceptor/logging.module';
+import { BrandsModule } from './brands/brands.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      namingStrategy: new SnakeNamingStrategy(),
+      logging: true,
+      timezone: process.env.DB_TIMEZONE,
+    }),
+    LoggingModule,
+    BrandsModule,
+    CustomersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- project-nest/src/main.ts
+++ project-nest/src/main.ts
@@ -1,8 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      forbidUnknownValues: false,
+    }),
+  );
+  app.enableCors();
+  app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle('bb')
+    .setDescription('The bb api description')
+    .setVersion('0.0.1')
+    .addTag('bb')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // localhost:port/api
+
+  await app.listen(process.env.PORT);
+  // await app.listen(3000);
 }
 bootstrap();
